7551c5cbf58155e4eebc0cd5c12f8a952022c3ec
italo <you@example.com>
2022-06-05 17:19:50 -0300

    feat: initial app structure

---
+++ src/main/java/passify/app/domain/Event.java
@@ -0,0 +1,30 @@
+package passify.app.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Event {
+    @Id
+    private UUID id;
+    private String name;
+    private String description;
+    private double price;
+    private double rate;
+
+    private String addressStreetName;
+    private String addressStreetNumber;
+    private String addressAdditional;
+    private String addressCity;
+    private String addressState;
+    private String addressZip;
+}
+
