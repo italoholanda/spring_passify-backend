e70be18de577fdd9d4f31f2cc74084f718970865
italo <you@example.com>
2022-06-06 05:57:56 -0300

    feat: event post/put mappers

---
+++ src/main/java/passify/app/mapper/EventMapper.java
@@ -0,0 +1,17 @@
+package passify.app.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import passify.app.domain.Event;
+import passify.app.request.EventPostRequestBody;
+import passify.app.request.EventPutRequestBody;
+
+@Mapper(componentModel = "spring")
+public abstract class EventMapper {
+    public static final EventMapper INSTANCE = Mappers.getMapper(EventMapper.class);
+    @Autowired
+    public abstract Event toEvent(EventPostRequestBody animePostRequestBody);
+    public abstract Event toEvent(EventPutRequestBody animePutRequestBody);
+}
