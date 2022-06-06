6334c2b3ff6dd1d50acb8d178953259fbf635ad9
italo <you@example.com>
2022-06-06 05:58:08 -0300

    feat: event services

--- src/main/java/passify/app/service/EventService.java
+++ src/main/java/passify/app/service/EventService.java
@@ -1,13 +1,21 @@
 package passify.app.service;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import passify.app.domain.Event;
-import passify.app.repository.EventRepository;
 
+import passify.app.domain.Event;
+import passify.app.exception.BadRequestException;
+import passify.app.mapper.EventMapper;
+import passify.app.repository.EventRepository;
+import passify.app.request.EventPostRequestBody;
+import passify.app.request.EventPutRequestBody;
+
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +28,30 @@ public class EventService {
 
     public List<Event> listAllNonPageable() {
         return eventRepository.findAll();
+    }
+
+    public List<Event> findByName(String name) {
+        return eventRepository.findByName(name);
+    }
+    public Event findByIdOrThrowBadRequest(UUID id) {
+        return eventRepository.findById(id)
+                .orElseThrow(() -> new BadRequestException("Event not found"));
+    }
+
+    @Transactional
+    public Event save(EventPostRequestBody eventPostRequestBody) {
+        Event event = EventMapper.INSTANCE.toEvent(eventPostRequestBody);
+        return eventRepository.save(event);
+    }
+
+    public void delete(UUID id) {
+        eventRepository.delete(findByIdOrThrowBadRequest(id));
+    }
+
+    public void replace(EventPutRequestBody eventPutRequestBody) {
+        Event savedEvent = findByIdOrThrowBadRequest(eventPutRequestBody.getId());
+        Event event = EventMapper.INSTANCE.toEvent(eventPutRequestBody);
+        event.setId(savedEvent.getId());
+        eventRepository.save(event);
     }
 }
