29309eb5005a5603fe1193e9456f23e65f8f02fd
italo <you@example.com>
2022-06-06 05:42:57 -0300

    feat: bad request exception details class

---
+++ src/main/java/passify/app/exception/BadRequestExceptionDetails.java
@@ -0,0 +1,9 @@
+package passify.app.exception;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class BadRequestExceptionDetails extends ExceptionDetails {
+}
