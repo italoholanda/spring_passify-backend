60401416b6a6b09c329b98e5d52c8c2ac6881a2a
italo <italogholanda@gmail.com>
2022-06-07 21:37:21 -0300

    refactor: changed uuid by long jpa index

--- src/main/java/passify/app/domain/Event.java
+++ src/main/java/passify/app/domain/Event.java
@@ -1,20 +1,27 @@
 package passify.app.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.UUID;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @Entity
+@Builder
+@NoArgsConstructor
 public class Event {
     @Id
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "The event name cannot be empty")
     private String name;
     private String description;
     private double price;
